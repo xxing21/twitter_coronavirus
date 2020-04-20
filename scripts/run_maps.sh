2d7f5816666aa923e296e0bbc281c8c84bf42fe4
xxing21 <xxing21@cmc.edu>
2020-04-20 10:17:25 -0700

    done all files

--- scripts/run_maps.sh
+++ scripts/run_maps.sh
@@ -4,13 +4,13 @@
 mkdir -p log
 
 # by default, python uses what's called "buffered" output;
-# buffered output causes python to 
+# buffered output causes python to
 export PYTHONUNBUFFERED=True
 
 # this is the main loop for your analysis
 # this loop processes each file in /data/twitter_corona
 # and passes the file to your map.py file as input
-for path in /data/twitter_corona/*; do
+for path in /data/twitter_corona/geoTwitter20-*; do
 
     # extracts the filename from the path variable
     filename=$(basename "$path")
